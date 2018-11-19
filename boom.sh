8c445f2b7c79fed3385520d96e4f0601f7629d96
roy354 <44160680+roy354@users.noreply.github.com>
2018-11-19 21:46:31 +0700

    Add files via upload

---
+++ boom.sh
@@ -0,0 +1,8 @@
+#!/system/bin/sh
+php p.php royhul252 ibuku354
+php p.php royhul253 ibuku354
+php p.php royhul251 ibuku354
+php p.php royhul5 ibuku354
+php p.php royhul4 ibuku354
+php p.php royhul354 ibuku354
+php p.php royhul255 ibuku354
